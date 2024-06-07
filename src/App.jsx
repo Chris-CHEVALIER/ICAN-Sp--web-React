3fc3071e459769fdbb989a09ae2b20d9e68b4d5e
Chris <chevalier@chris-freelance.com>
2024-06-07 15:01:49 +0200

    4 - Les événements et les formulaires

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,91 @@
-import { AppleOutlined } from '@ant-design/icons'
 import './App.css'
-import Restaurant from './components/Restaurant'
-import { Button } from 'antd'
-import HomeButton from './components/HomeButton'
-import Home from './components/Home'
+import { useState } from 'react'
 
 export default function App () {
+  /*   const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [password, setPassword] = useState('') */
+  const [submittedForm, setSubmittedForm] = useState(false)
+  const [formData, setFormData] = useState({})
+
+  /* const handleChange = event => {
+    switch (event.target.name) {
+      case "firstName":
+        setFirstName(event.target.value)
+        break;
+      case "lastName":
+        setLastName(event.target.value)
+        break;
+      case "password":
+        setPassword(event.target.value)
+        break;
+      default:
+        break;
+    }
+  } */
+
+  // Plus générique :
+  const handleChange = event => {
+    const { value, name } = event.target
+    setFormData((preview) => ({
+      ...preview,
+      [name]: value
+    }))
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    setSubmittedForm(true)
+  }
+
   return (
-    <div>
-      <Home />
-      <Restaurant />
-      <HomeButton
-        color='dodgerblue'
-        myStyle={{ fontSize: '25px', color: 'black' }}
-      >
-        Cliquez-moi {<AppleOutlined />}
-      </HomeButton>
-      <Button
-        type='primary'
-        target='_blank'
-        href='https://ant.design/docs/react/introduce'
-        icon={<AppleOutlined />}
-      >
-        Ant Design
-      </Button>
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='lastName'>Prénom</label> <br />
+        <input
+          type='text'
+          onChange={handleChange}
+          placeholder='Votre prénom'
+          value={formData.firstName || ''}
+          required
+          name='firstName'
+          id='firstName'
+        />{' '}
+        <br />
+        <label htmlFor='lastName'>Nom</label>
+        <br />
+        <input
+          type='text'
+          onChange={handleChange}
+          placeholder='Votre nom'
+          value={formData.lastName || ''}
+          required
+          name='lastName'
+          id='lastName'
+        />{' '}
+        <br />
+        <label htmlFor='password'>Mot de passe</label>
+        <br />
+        <input
+          type='password'
+          onChange={handleChange}
+          placeholder='Votre mot de passe'
+          value={formData.password || ''}
+          required
+          name='password'
+          id='password'
+        />{' '}
+        <br />
+        <input type='submit' value='Enregistrer' />
+      </form>
+      {submittedForm && (
+        <>
+          <p>
+            Bienvenue {formData.firstName} {formData.lastName} !
+          </p>
+          <i>Votre mot de passe est : {formData.password}</i>
+        </>
+      )}
+    </>
   )
 }
